0c4de028e7879777fbd3431638de938fea73f551
Eric Ji <eric.ji3333@gmail.com>
2021-11-27 16:19:07 -0600

    WHOOOO FIXED TRANSITION CLICK ON LETTER

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,68 +9,179 @@ import $ from 'jquery';
 import { CSSTransition, SwitchTransition } from "react-transition-group";
 
 function Home(){
-    const [e,setE] = useState(false);
-    const [r,setR] = useState(false);
-    const [i,setI] = useState(false);
-    const [c,setC] = useState(false);
-    const eArray = ['X','T','R','A','T','E','R','R','E','S','T','R','I','A','L'];
+    var e = false;
+    var r = false;
+    var i = false;
+    var c = false;
+    // const [e,setE] = useState(false);
+    // const [r,setR] = useState(false);
+    // const [i,setI] = useState(false);
+    // const [c,setC] = useState(false);
+    const eArray = ['E','X','T','R','A','T','E','R','R','E','S','T','R','I','A','L'];
+    const rArray = ['R','E','S','P','O','N','S','I','V','E'];
+    const iArray = ['I','M','A','G','I','N','A','T','I','V','E'];
+    const cArray = ['C','O','O','L'];
 
-    useEffect(()=>{
-        console.log(e);
-        if(e){
-                $('.E').addClass('active');
+    function handleClick(letter){
+        if(letter == "E"){
+            e = !e;
+            console.log(e)
+            if(e){
+                //get the welcome msg element
+                var $all_msg = $('.E');
+                //get a list of letters from the welcome text
+                //clear the welcome text msg
+                $all_msg.text("");
+                //loop through the letters in the $wordList array
+                $.each(eArray, function(idx, elem) {
+                    //create a span for the letter and set opacity to 0
+                    var newEL = $("<span/>").text(elem).css({
+                    opacity: 0
+                    });
+                    //append it to the welcome message
+                    newEL.appendTo($all_msg);
+                    //set the delay on the animation for this element
+                    newEL.delay(idx * 70);
+                    //animate the opacity back to full 1
+                    newEL.animate({
+                    opacity: 1
+                    }, 1100);
+                });
                 $('.EletterCards').addClass('active');
-                $('.EletterCards').slideDown(1000, function(){
+                $('.EletterCards.active').slideDown(1000, function(){
                     // $('.home').css('height', '100%');
                 });
                 
         } else{
+                var $all_msg = $('.E');
+                $all_msg.text("");
+                var newEL = $("<span/>").text('E').css({
+                opacity: 0
+                });
+                newEL.appendTo($all_msg);
+                newEL.animate({
+                opacity: 1
+                }, 1100);
                 $('.E.active').removeClass('active');
                 // $('.home').css('height', '100vh');
                 $('.EletterCards.active').slideUp(1000, function(){
                     $('.EletterCards').removeClass('active');
                 });
         }
-        if(r){
-            $('.R').addClass('active');
-            $('.RletterCards').addClass('active');
-            $('.RletterCards.active').slideDown(1000, function(){
-                // $('.home').css('height', '100%');
-            });
-        } else{
-                $('.R.active').removeClass('active');
-                // $('.home').css('height', '100vh');
-                $('.RletterCards.active').slideUp(1000, function(){
-                    $('.RletterCards').removeClass('active');
+        }
+        if(letter == "R"){
+            r = !r;
+            if(r){
+                var $r_message = $('.R');
+                $r_message.text("");
+                $.each(rArray, function(idx, elem) {
+                    var newR = $("<span/>").text(elem).css({
+                    opacity: 0
+                    });
+                    newR.appendTo($r_message);
+                    newR.delay(idx * 70);
+                    newR.animate({
+                    opacity: 1
+                    }, 1100);
                 });
-        }
-        if(i){
-            $('.I').addClass('active');
-            $('.IletterCards').addClass('active');
-            $('.IletterCards.active').slideDown(1000, function(){
-                // $('.home').css('height', '100%');
-            });
-        } else{
-            $('.I.active').removeClass('active');
-            // $('.home').css('height', '100vh');
-                $('.IletterCards.active').slideUp(1000, function(){
-                    $('.IletterCards').removeClass('active');
+                $('.R').addClass('active');
+                $('.RletterCards').addClass('active');
+                $('.RletterCards.active').slideDown(1000, function(){
+                    // $('.home').css('height', '100%');
                 });
+            } else{
+                var $r_message = $('.R');
+                $r_message.text("");
+                var newR = $("<span/>").text('R').css({
+                opacity: 0
+                });
+                newR.appendTo($r_message);
+                newR.animate({
+                opacity: 1
+                }, 1100);
+                    $('.R.active').removeClass('active');
+                    // $('.home').css('height', '100vh');
+                    $('.RletterCards.active').slideUp(1000, function(){
+                        $('.RletterCards').removeClass('active');
+                    });
+            }
         }
-        if(c){
-            $('.C').addClass('active');
-            $('.CletterCards').addClass('active');
-            $('.CletterCards.active').slideDown(1000,function(){
-                // $('.home').css('height', '100%');
-            });
-        } else{
-            $('.C.active').removeClass('active');
-            // $('.home').css('height', '100vh');
-            $('.CletterCards.active').slideUp(1000, function(){
-                $('.CletterCards').removeClass('active');
-            });
+        if(letter == "I"){
+            i = !i;
+            if(i){
+                var $i_message = $('.I');
+                $i_message.text("");
+                $.each(iArray, function(idx, elem) {
+                    var newI = $("<span/>").text(elem).css({
+                    opacity: 0
+                    });
+                    newI.appendTo($i_message);
+                    newI.delay(idx * 70);
+                    newI.animate({
+                    opacity: 1
+                    }, 1100);
+                });
+                $('.I').addClass('active');
+                $('.IletterCards').addClass('active');
+                $('.IletterCards.active').slideDown(1000, function(){
+                    // $('.home').css('height', '100%');
+                });
+            } else{
+                var $i_message = $('.I');
+                $i_message.text("");
+                var newR = $("<span/>").text('I').css({
+                opacity: 0
+                });
+                newR.appendTo($i_message);
+                newR.animate({
+                opacity: 1
+                }, 1100);
+                    $('.I.active').removeClass('active');
+                    // $('.home').css('height', '100vh');
+                    $('.IletterCards.active').slideUp(1000, function(){
+                        $('.IletterCards').removeClass('active');
+                    });
+            }
         }
-        })
+        if(letter == "C"){
+            c = !c;
+            if(c){
+                var $c_message = $('.C');
+                $c_message.text("");
+                $.each(cArray, function(idx, elem) {
+                    var newC = $("<span/>").text(elem).css({
+                    opacity: 0
+                    });
+                    newC.appendTo($c_message);
+                    newC.delay(idx * 70);
+                    newC.animate({
+                    opacity: 1
+                    }, 1100);
+                });
+                $('.C').addClass('active');
+                $('.CletterCards').addClass('active');
+                $('.CletterCards.active').slideDown(1000, function(){
+                    // $('.home').css('height', '100%');
+                });
+            } else{
+                var $c_message = $('.C');
+                var $wordList = $c_message.text().split("");
+                $c_message.text("");
+                var newC = $("<span/>").text('C').css({
+                opacity: 0
+                });
+                newC.appendTo($c_message);
+                newC.animate({
+                opacity: 1
+                }, 1100);
+                    $('.C.active').removeClass('active');
+                    // $('.home').css('height', '100vh');
+                    $('.CletterCards.active').slideUp(1000, function(){
+                        $('.CletterCards').removeClass('active');
+                    });
+            }
+        }
+    }
     return(
         <div className="home">
             <div className="name-container row justify-content-center">
@@ -79,19 +190,19 @@ function Home(){
                 - setInterval equal to transition time
                 - add data-attribute to each letter for the word */}
 
-                <p className="E" onClick={() => setE(e => !e)}>Extraterrestrial</p>
+                <p className="E" onClick={() => handleClick('E')}>E</p>
                 <div className='EletterCards'>
                     <E/>
                 </div>
-                <span className="R" onClick={() => setR(!r)}>{r ? 'Responsive' : 'R'}</span>
+                <span className="R" onClick={() => handleClick('R')}>R</span>
                 <div className='RletterCards'>
                     <R/>
                 </div>
-                <span className="I" onClick={() => setI(!i)}>{i ? 'Imaginative' : 'I'}</span>
+                <span className="I" onClick={() => handleClick('I')}>I</span>
                 <div className='IletterCards'>
                     <I/>
                 </div>
-                <span className="C" onClick={() => setC(!c)}>{c ? 'Cool' : 'C'}</span>
+                <span className="C" onClick={() => handleClick('C')}>C</span>
                 <div className='CletterCards'>
                     <C/>
                 </div>
